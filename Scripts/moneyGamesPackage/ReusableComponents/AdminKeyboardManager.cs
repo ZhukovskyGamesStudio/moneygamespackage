7136e89d76d93efc190eed7cede686b6e319291f
GrafEnters <graf.enters@gmail.com>
2024-09-05 13:33:16 +0300

    Added adminKeyboardManager

---
+++ Scripts/moneyGamesPackage/ReusableComponents/AdminKeyboardManager.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AdminKeyboardManager : Singleton<AdminKeyboardManager> {
+    [SerializeField]
+    private KeyCode _controlKeycode = KeyCode.LeftAlt;
+
+    private Dictionary<KeyCode, Action> _keyboardCommands = new Dictionary<KeyCode, Action>();
+
+    protected override void OnFirstInited() {
+        base.OnFirstInited();
+        _keyboardCommands = new Dictionary<KeyCode, Action>() {
+            { KeyCode.R, FullReloadGame },
+            { KeyCode.T, ReloadCurrentScene }
+        };
+    }
+
+    private void FullReloadGame() {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("PlayerSelectScene");
+    }
+
+    private void ReloadCurrentScene() {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void Update() {
+        CheckKeyboardCommands();
+    }
+
+    private void CheckKeyboardCommands() {
+        foreach (KeyValuePair<KeyCode, Action> kvp in _keyboardCommands) {
+            if (Input.GetKey(_controlKeycode) && Input.GetKeyDown(kvp.Key)) {
+                kvp.Value.Invoke();
+            }
+        }
+    }
+}
