769b395689f9d92c3b9a9f3bf83776afbd9646e0
GrafEnters <graf.enters@gmail.com>
2025-04-19 00:53:54 +0200

    mgp updated

--- Scripts/YGPlugin/YGWrapper.cs
+++ Scripts/YGPlugin/YGWrapper.cs
@@ -1,14 +1,13 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using YG;
-#if UNITY_WEBGL
+#if UNITY_WEBGL && YG_WEBGL
 using YG;
 #endif
 
 public static class YGWrapper {
     public static void SendYandexMetrica(string eventName, string parameter) {
-#if UNITY_WEBGL
+#if UNITY_WEBGL && YG_WEBGL
         YandexMetrica.Send(eventName, new Dictionary<string, string>() { { eventName, parameter } });
 #else
         Debug.Log($"Disabled on current platform: Yandex metrica event: {eventName} amount: {parameter}");
@@ -16,7 +15,7 @@ public static class YGWrapper {
     }
 
     public static void SendLeaderboardScore(string leaderboardId, int amount) {
-#if UNITY_WEBGL
+#if UNITY_WEBGL && YG_WEBGL
         YandexGame.NewLeaderboardScores(leaderboardId, amount);
 #else
         Debug.Log($"Disabled on current platform: Leaderboard id: {leaderboardId} amount: {amount}");
@@ -24,12 +23,12 @@ public static class YGWrapper {
     }
 
     public static long GetServerTime() {
-#if UNITY_WEBGL
+#if UNITY_WEBGL && YG_WEBGL
         return YandexGame.ServerTime();
 #else
-        long res = RealTimeTools.GetTimeSpanMilliseconds(DateTime.Now  - DateTime.UnixEpoch);
+        long res = RealTimeTools.GetTimeSpanMilliseconds(DateTime.Now - DateTime.UnixEpoch);
         long addedAdminTime = RealTimeTools.GetTimeSpanMilliseconds(TimeSpan.FromHours(PlayerPrefs.GetFloat("AdminAddedHours")));
-       // Debug.Log($"Disabled on current platform: return DateTime.now: {res}, fixed Yandex time: {YandexGame.ServerTime()}");
+        // Debug.Log($"Disabled on current platform: return DateTime.now: {res}, fixed Yandex time: {YandexGame.ServerTime()}");
         return res + addedAdminTime;
 #endif
     }
